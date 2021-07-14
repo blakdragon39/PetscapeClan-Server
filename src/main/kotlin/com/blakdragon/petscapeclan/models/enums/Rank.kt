04befe3f18d4990925d7937c926ca3e2169110d3
Jenna Link <jenna.hartley@pushinteractions.com>
2021-07-14 11:50:18 -0600

    Added points to Ranks

--- src/main/kotlin/com/blakdragon/petscapeclan/models/enums/Rank.kt
+++ src/main/kotlin/com/blakdragon/petscapeclan/models/enums/Rank.kt
@@ -2,51 +2,63 @@ package com.blakdragon.petscapeclan.models.enums
 
 enum class Rank(
     val label: String,
-    val order: Int
+    val order: Int,
+    val points: Int
 ) {
     Bronze(
         label = "Bronze",
-        order = 0
+        order = 0,
+        points = 1
     ),
     Iron(
         label = "Iron",
-        order = 1
+        order = 1,
+        points = 10
     ),
     Steel(
         label = "Steel",
-        order = 2
+        order = 2,
+        points = 20
     ),
     Gold(
         label = "Gold",
-        order = 3
+        order = 3,
+        points = 35
     ),
     Mithril(
         label = "Mithril",
-        order = 4
+        order = 4,
+        points = 45
     ),
     Adamant(
         label = "Adamant",
-        order = 5
+        order = 5,
+        points = 55
     ),
     Rune(
         label = "Rune",
-        order = 6
+        order = 6,
+        points = 70
     ),
     Dragon(
         label = "Dragon",
-        order = 7
+        order = 7,
+        points = 85
     ),
     Admin(
         label = "Admin",
-        order = 8
+        order = 8,
+        points = Int.MAX_VALUE
     ),
     DeputyOwner(
         label = "Deputy Owner",
-        order = 9
+        order = 9,
+        points = Int.MAX_VALUE
     ),
     Owner(
         label = "Owner",
-        order = 10
+        order = 10,
+        points = Int.MAX_VALUE
     );
 
     fun toResponse(): RankResponse = RankResponse(this)
@@ -57,4 +69,5 @@ class RankResponse(
 ) {
     val label = rank.label
     val order = rank.order
+    val points = rank.points
 }
